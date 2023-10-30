582991aec71eda5a7d9e6894702b237600e67997
MikkelRonni <119924602+MikkelRonni@users.noreply.github.com>
2023-10-30 20:59:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Define MQTT broker settings
+mqtt_broker = "mosquittobroker.westeurope.azurecontainer.io"
+mqtt_port = 1883
+mqtt_topics = ["MIR/temp", "MIR/humi"]
+
+# Initialize data dictionaries for each topic
+mqtt_data = {topic: '' for topic in mqtt_topics}
+
+def on_message(client, userdata, message):
+    topic = message.topic
+    data = message.payload.decode('utf-8')
+    mqtt_data[topic] = data  # Update data dictionary
+    socketio.emit('mqtt_message', {'topic': topic, 'data': data})
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_message = on_message
+mqtt_client.connect(mqtt_broker, mqtt_port, 60)
+mqtt_client.loop_start()  # Start the MQTT client loop
+
+@socketio.on('connect')
+def handle_connect():
+    for topic in mqtt_topics:
+        mqtt_client.subscribe(topic)
+    socketio.emit('mqtt_connected', {'data': 'Connected to MQTT broker'})
 
 @app.route('/')
-def home():
-   return render_template('index.html')
+def index():
+    return render_template('index.html', mqtt_data=mqtt_data)
 
 if __name__ == '__main__':
-   app.run()
-
-# MQTT connection details
-broker_address = "mosquittobroker.westeurope.azurecontainer.io"
-broker_port = 1883  # Change this to the correct port
-topic_temp = "MIR/temp"
-topic_humi = "MIR/humi"
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe(topic_temp)
-    client.subscribe(topic_humi)
-
-def on_message(client, userdata, msg):
-    print("Received message on topic: " + msg.topic)
-    # You can send the message data to the HTML template via WebSockets or any other method of your choice
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect(broker_address, broker_port)
-
-client.loop_start()
-
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
